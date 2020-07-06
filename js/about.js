d70b044def7782f12487fb7821c7a9f5f1c91141
Anand <anandstack@gmail.com>
2020-07-06 17:14:21 +0530

    New feature

--- js/about.js
+++ js/about.js
@@ -2,3 +2,7 @@
 function clickFromAbout() {
     console.log("Click from About");
 }
+
+function newFunForAbout() {
+    console.log("hello, I am new for About page");
+}
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,8 @@
 function clickFromHome() {
   console.log("Click from Home");
 }
+
+
+function newFunForHome() {
+  console.log("hello, I am new for Home page");
+}
